7071ba14b6b460bc646e247ba4daf1d09a5d6784
Vaibhav-23 <Vaibhav.Dandapanthula@iiitb.org>
2024-04-30 02:37:59 +0530

    updated the socket path

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({children})=>{
 
     useEffect(()=>{
         if(authUser){
-            const socket = io("http://localhost:5000",{
+            const socket = io("https://chatapp-jjhw.onrender.com",{
                 query:{
                     userId: authUser._id,    
                 }
